224f15722c140882f6a5840be1558e6e54d1f25b
Prashwar Mishra <prashwarmishra@gmail.com>
2021-06-05 21:44:51 +0530

    persisting user-1

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 
 import { fetchPosts } from '../actions/posts';
 import { Home, Navbar, Page404, Login, Signup } from './';
@@ -9,6 +10,12 @@ import { Home, Navbar, Page404, Login, Signup } from './';
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts());
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      const user = jwtDecode(token);
+      console.log('Token payload is: ', user);
+    }
   }
 
   render() {
